e77a3da32682be6a071d92030934c1cdb7ce5a77
tnt120 <apaj04@gmail.com>
2023-02-13 20:51:30 +0100

    Added ContextProvider

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Line, Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+
+import { useStateContext } from './contexts/ContextProvider';
+
 import './App.css';
 import { Side } from '@syncfusion/ej2/svg-base';
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
 
   return (
     <div>
